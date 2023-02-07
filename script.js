fc1770fd1d121b3f6688fb6512a83da71fe44121
calforcal <mcalla242@gmail.com>
2023-02-07 10:46:39 -0700

    Pushing so i dont lose progress

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ const gridContainer = document.querySelector(".gridContainer");
 //Makes one column and NUM number of rows nested in the column
 function makeColumnRows (num) {
     let divColumn = document.createElement("div");
-    divColumn.textContent = "I'm a Column";
+    divColumn.textContent = " ";
     divColumn.className = "column";
 
     let divRow = document.createElement("div");
-    divRow.textContent = "I'm a row";
+    divRow.textContent = " ";
     divRow.className = "row";
 
     gridContainer.append(divColumn);
 
     for (let i = 0; i < num; i++) {
         divRow = document.createElement("div");
-        divRow.textContent = "I'm a row";
+        divRow.textContent = "";
         divRow.className = "row";
         divColumn.appendChild(divRow);
     }
-
 }
 
 //Makes the grid based on a certain number of columns / rows.
 function makeGrid (num1, num2) {
     for (let j = 0; j < num1; j++) {
-        makeColumnRows(num2);
+        makeColumnRows(num2 - 1);
     };
 }
 
-makeGrid(16, 16);
+makeGrid(100, 100);
+
+console.log(document.getElementsByClassName("row"));
